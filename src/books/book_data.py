aba96c3bff0dc4cd384850989d6fe0708d215214
Mohammad <mohammad70623@gmail.com>
2026-04-18 23:36:16 +0600

    add dummy book data

---
+++ src/books/book_data.py
@@ -0,0 +1,93 @@
+
+books = [ 
+    {
+        "id": 1,
+        "title": "Clean Code",
+        "author": "Robert C. Martin",
+        "publisher": "Prentice Hall",
+        "published_date": "2008-08-01",
+        "page_count": 464,
+        "language": "English"
+    },
+    {
+        "id": 2,
+        "title": "The Pragmatic Programmer",
+        "author": "Andrew Hunt, David Thomas",
+        "publisher": "Addison-Wesley",
+        "published_date": "1999-10-30",
+        "page_count": 352,
+        "language": "English"
+    },
+    {
+        "id": 3,
+        "title": "Design Patterns: Elements of Reusable Object-Oriented Software",
+        "author": "Erich Gamma, Richard Helm, Ralph Johnson, John Vlissides",
+        "publisher": "Addison-Wesley",
+        "published_date": "1994-10-21",
+        "page_count": 395,
+        "language": "English"
+    },
+    {
+        "id": 4,
+        "title": "Refactoring: Improving the Design of Existing Code",
+        "author": "Martin Fowler",
+        "publisher": "Addison-Wesley",
+        "published_date": "1999-07-08",
+        "page_count": 431,
+        "language": "English"
+    },
+    {
+        "id": 5,
+        "title": "Introduction to Algorithms",
+        "author": "Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest, Clifford Stein",
+        "publisher": "MIT Press",
+        "published_date": "2009-07-31",
+        "page_count": 1312,
+        "language": "English"
+    },
+    {
+        "id": 6,
+        "title": "Structure and Interpretation of Computer Programs",
+        "author": "Harold Abelson, Gerald Jay Sussman",
+        "publisher": "MIT Press",
+        "published_date": "1996-07-25",
+        "page_count": 657,
+        "language": "English"
+    },
+    {
+        "id": 7,
+        "title": "You Don’t Know JS Yet",
+        "author": "Kyle Simpson",
+        "publisher": "O'Reilly Media",
+        "published_date": "2020-01-28",
+        "page_count": 278,
+        "language": "English"
+    },
+    {
+        "id": 8,
+        "title": "Head First Design Patterns",
+        "author": "Eric Freeman, Elisabeth Robson",
+        "publisher": "O'Reilly Media",
+        "published_date": "2004-10-25",
+        "page_count": 694,
+        "language": "English"
+    },
+    {
+        "id": 9,
+        "title": "Clean Architecture",
+        "author": "Robert C. Martin",
+        "publisher": "Prentice Hall",
+        "published_date": "2017-09-20",
+        "page_count": 432,
+        "language": "English"
+    },
+    {
+        "id": 10,
+        "title": "The Mythical Man-Month",
+        "author": "Frederick P. Brooks Jr.",
+        "publisher": "Addison-Wesley",
+        "published_date": "1975-01-01",
+        "page_count": 322,
+        "language": "English"
+    }
+]
