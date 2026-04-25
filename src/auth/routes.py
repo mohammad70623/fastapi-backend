6a5dda0e2d18983929682cfedaf311cd07a99520
Mohammad <mohammad70623@gmail.com>
2026-04-26 01:52:32 +0600

    implement JWT authentication with login endpoint

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -1,14 +1,17 @@
 from fastapi import APIRouter, Depends, status
-from .schemas import UserCreateModel, UserModel
+from .schemas import UserCreateModel, UserModel, UserLoginModel
 from .service import UserService
 from src.db.database import get_session
 from sqlmodel.ext.asyncio.session import AsyncSession
 from fastapi.exceptions import HTTPException
-
+from .utils import create_access_token, decode_token, verify_password
+from datetime import timedelta
+from fastapi.responses import JSONResponse 
 
 auth_router = APIRouter()
 user_service = UserService()
 
+REFRESH_TOKEN_EXPIRY = 2
 
 @auth_router.post('/signup', response_model=UserModel, status_code=status.HTTP_201_CREATED)
 async def create_user_Account(
@@ -27,5 +30,50 @@ async def create_user_Account(
     
     return new_user
 
+@auth_router.post("/login")
+async def login_user(
+    login_data: UserLoginModel, session: AsyncSession = Depends(get_session)
+):
+    email = login_data.email
+    password = login_data.password
+
+    user = await user_service.get_user_by_email(email, session)
+    if user is not None:
+        password_valid = verify_password(password, user.password_hash)
+
+        if password_valid:
+            access_token = create_access_token(
+                user_data={
+                    'email': user.email,
+                    'user_uid': str(user.uid)
+                }
+            )
+
+            refresh_token = create_access_token(
+                user_data={
+                    'email': user.email,
+                    'user_uid': str(user.uid)
+                },
+                refresh=True,
+                 expiry=timedelta(days=REFRESH_TOKEN_EXPIRY)
+            )
+
+            return JSONResponse(
+                content ={
+                    "message": "Login Successful",
+                    "access_token": access_token,
+                    "refresh_token": refresh_token,
+                    "user":{
+                        "email": user.email,
+                        "uid": str(user.uid)
+                    }
+
+                }
+            )
+        
+    raise HTTPException(
+        status_code=status.HTTP_403_FORBIDDEN,
+        detail="Invalid Email or Passwod"
+    )
 
 
--- src/auth/utils.py
+++ src/auth/utils.py
@@ -1,4 +1,11 @@
 import bcrypt
+from datetime import timedelta, datetime
+from src.config import config
+import jwt
+import uuid
+import logging
+
+ACCESS_TOKEN_EXPIRY = 3600
 
 def generate_passwd_hash(password: str) -> str:
     pwd_bytes = password.encode('utf-8')
@@ -11,3 +18,32 @@ def verify_password(password: str, hash: str) -> bool:
     hash_bytes = hash.encode('utf-8')
     return bcrypt.checkpw(pwd_bytes, hash_bytes)
 
+
+def create_access_token(user_data: dict, expiry:timedelta = None,refresh: bool = False):
+
+    payload={}
+    payload['user'] = user_data
+    payload['exp'] = datetime.now()+ (expiry if expiry is not None else timedelta(seconds=ACCESS_TOKEN_EXPIRY))
+    payload['jti'] = str(uuid.uuid4())
+    payload['refresh'] = refresh
+    token = jwt.encode(
+        payload=payload,
+        key=config.JWT_SECRET,
+        algorithm=config.JWT_ALGORITHM
+    )  
+    return token
+
+def decode_token(token: str)->dict:
+    try:
+        token_data = jwt.decode(
+            jwt=token,
+            key=config.JWT_SECRET,
+            algorithms=[config.JWT_ALGORITHM]
+        )
+
+        return token_data
+    
+    except jwt.PyJWTError as e:
+        logging.exception(e)
+        return None
+    
