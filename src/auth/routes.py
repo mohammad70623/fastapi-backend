b0bd67862254e280274b7ce672b8cbcda31fceab
Mohammad <mohammad70623@gmail.com>
2026-04-28 15:59:47 +0600

    Implement refresh token for regaining access

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -5,9 +5,9 @@ from src.db.database import get_session
 from sqlmodel.ext.asyncio.session import AsyncSession
 from fastapi.exceptions import HTTPException
 from .utils import create_access_token, decode_token, verify_password
-from datetime import timedelta
+from datetime import timedelta, datetime
 from fastapi.responses import JSONResponse 
-
+from .dependencies import RefreshTokenBearer
 
 auth_router = APIRouter()
 user_service = UserService()
@@ -77,4 +77,17 @@ async def login_user(
         detail="Invalid Email or Passwod"
     )
 
+@auth_router.get('/refresh_token')
+async def get_new_access_token(token_details:dict=Depends(RefreshTokenBearer())):
+    expiry_timestamp = token_details['exp']
 
+    if datetime.fromtimestamp(expiry_timestamp) > datetime.now():
+        new_access_token = create_access_token(user_data = token_details["user"])
+        return JSONResponse(content ={"access_token": new_access_token})
+    
+    raise HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid or expired token"
+    )
+
+
+    return {} 
